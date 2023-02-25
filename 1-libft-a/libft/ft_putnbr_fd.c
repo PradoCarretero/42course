f0d07ae53d23aec9d4cb27008daf3f62e3fd2c8a
PradoCarretero <p.carreteroayala@gmail.com>
2023-02-25 17:17:20 +0100

    add ft_putnbr_fd

--- 1-libft-a/libft/ft_putnbr_fd.c
+++ 1-libft-a/libft/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: pcarrete <pcarrete@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/25 16:35:11 by pcarrete          #+#    #+#             */
-/*   Updated: 2023/02/25 16:35:48 by pcarrete         ###   ########.fr       */
+/*   Updated: 2023/02/25 17:15:32 by pcarrete         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,24 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
+	int	c;
 
+	if (n < 0)
+	{
+		if (n == -2147483648)
+		{
+		ft_putstr_fd("-2147483648", fd);
+		return;
+		}
+		n = n * (-1);
+		ft_putchar_fd('-', fd);
+	}
+	if (n <= 9)
+		c = n;
+	else
+	{
+		c = n % 10;
+		ft_putnbr_fd(n / 10, fd);
+	}
+		ft_putchar_fd(c + '0', fd);
 }
